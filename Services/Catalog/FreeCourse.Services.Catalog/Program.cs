58a8d7895c4e8dcd83de2e20065e4b9cd36bbb8a
Serhat Ayata <srhtayata@gmail.com>
2022-07-21 17:39:01 +0300

    210722 - FreeCourse.Services.Catalog Services and Controllers added

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,9 +1,31 @@
+using FreeCourse.Services.Catalog.Mapping;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager Configuration = builder.Configuration;
+IWebHostEnvironment Environment = builder.Environment;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+#region AutoMapper
+builder.Services.AddAutoMapper(typeof(GeneralMapping).Assembly);
+#endregion
+#region IOption
+builder.Services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+#endregion
+#region DI
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICourseService, CourseService>();
+
+#endregion
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
