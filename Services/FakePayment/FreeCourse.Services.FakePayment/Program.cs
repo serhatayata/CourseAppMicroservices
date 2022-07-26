b2b7b344f238bde813e925295625782d217a0aa3
Serhat Ayata <srhtayata@gmail.com>
2022-07-26 15:46:49 +0300

    FakePayment microservice completed

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager Configuration = builder.Configuration;
+IWebHostEnvironment Environment = builder.Environment;
+// Add services to the container.
+
+#region Authentication
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = Configuration["IdentityServerURL"];
+    options.Audience = "resource_payment";
+    options.RequireHttpsMetadata = false;
+});
+#endregion
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
